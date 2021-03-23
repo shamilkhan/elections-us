10f2fd80e7fc85be691b47ab208293b72c40c4ab
Шамильхан Ахметзянов <shamilkhan@MacBook-Pro-Samilhan.local>
2021-03-23 18:32:53 +0300

    Add lazy data loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 // @ts-nocheck
 import "./App.css";
-import React from 'react';
-import DeckGL from '@deck.gl/react';
-import { StaticMap } from 'react-map-gl';
-import { GeoJsonLayer } from '@deck.gl/layers';
-
-import UsStates from './data/state.json';
-import UsCounties from './data/next.json';
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { StaticMap } from "react-map-gl";
+import { GeoJsonLayer } from "@deck.gl/layers";
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
@@ -17,25 +14,31 @@ const INITIAL_VIEW_STATE = {
   latitude: 39.510819,
   zoom: 3.5,
   pitch: 0,
-  bearing: 0
+  bearing: 0,
 };
 
 const getColor = (f) => {
-  if (!f.properties.VOTES || !f.properties.VOTES.percentage20_Donald_Trump || !f.properties.VOTES.percentage20_Joe_Biden) {
+  if (
+    !f.properties.VOTES ||
+    !f.properties.VOTES.percentage20_Donald_Trump ||
+    !f.properties.VOTES.percentage20_Joe_Biden
+  ) {
     return [100, 100, 100, 100];
   }
   const trampVotes = Number(f.properties.VOTES.percentage20_Donald_Trump);
   const isRepablic = trampVotes > 0.5;
   const bidenVotes = Number(f.properties.VOTES.percentage20_Joe_Biden);
 
-  return isRepablic ? [200, 0, 0, 400 * Math.pow(trampVotes, 2.7)] : [0, 160, 180, 400 * Math.pow(bidenVotes, 2.7)]
+  return isRepablic
+    ? [200, 0, 0, 400 * Math.pow(trampVotes, 2.7)]
+    : [0, 160, 180, 400 * Math.pow(bidenVotes, 2.7)];
 };
 
 function App() {
   const [isDark, setDark] = React.useState(true);
   const countyLayer = new GeoJsonLayer({
-    id: 'geojson-layer',
-    data: UsCounties,
+    id: "geojson-layer",
+    data: "/data/next.json",
     pickable: true,
     stroked: false,
     filled: true,
@@ -49,21 +52,27 @@ function App() {
     getRadius: 100,
     getLineWidth: 100,
     highlightColor: ({ object: f }) => {
-      if (!f.properties.VOTES || !f.properties.VOTES.percentage20_Donald_Trump || !f.properties.VOTES.percentage20_Joe_Biden) {
+      if (
+        !f.properties.VOTES ||
+        !f.properties.VOTES.percentage20_Donald_Trump ||
+        !f.properties.VOTES.percentage20_Joe_Biden
+      ) {
         return [100, 100, 100, 300];
       }
       const trampVotes = Number(f.properties.VOTES.percentage20_Donald_Trump);
       const isRepablic = trampVotes > 0.5;
       const bidenVotes = Number(f.properties.VOTES.percentage20_Joe_Biden);
 
-      return isRepablic ? [200, 0, 0, 300 * Math.pow(trampVotes, 3)] : [0, 160, 180, 300 * Math.pow(bidenVotes, 3)]
+      return isRepablic
+        ? [200, 0, 0, 300 * Math.pow(trampVotes, 3)]
+        : [0, 160, 180, 300 * Math.pow(bidenVotes, 3)];
     },
     autoHighlight: true,
   });
 
   const stateLayer = new GeoJsonLayer({
-    id: 'geojson-layer-states',
-    data: UsStates,
+    id: "geojson-layer-states",
+    data: "/data/state.json",
     pickable: true,
     stroked: true,
     filled: false,
@@ -84,24 +93,32 @@ function App() {
       getTooltip={({ object }) => {
         if (!object) return;
         return {
-          html: `<div><h1>Hello</h1>world${JSON.stringify(object.properties)}</div>`
-        }
+          html: `<div><h1>Hello</h1>world${JSON.stringify(
+            object.properties
+          )}</div>`,
+        };
       }}
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
     >
-      <div style={{
-        "backgroundColor": "#fff",
-        "position": "absolute",
-        "width": "100%",
-        "top": 0,
-        "border": "1px solid #000",
-      }}>
+      <div
+        style={{
+          backgroundColor: "#fff",
+          position: "absolute",
+          width: "100%",
+          top: 0,
+          border: "1px solid #000",
+        }}
+      >
         filters:
         <label>
           Dark theme
-        <input type="checkbox" checked={isDark} onChange={() => setDark(!isDark)} />
+          <input
+            type="checkbox"
+            checked={isDark}
+            onChange={() => setDark(!isDark)}
+          />
         </label>
       </div>
       <StaticMap
