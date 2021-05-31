0560bab2f300bbb7b3af63e549e138089d941ab8
Artem <50479756+artemiyo@users.noreply.github.com>
2021-05-31 21:08:07 +0300

    IN-2: Show detailed information about voting by county (#33)
    * IN-2: added table with voting info
    
    * IN-2: deleted check icon
    
    * IN-2: changed Table to TableBuilder
    
    * IN-2: add minor fixes
    
    * IN-2: add newline at the end

--- src/countyData/index.tsx
+++ src/countyData/index.tsx
@@ -1,9 +1,44 @@
 // @ts-ignore
 import React from "react";
-import { Card, StyledBody } from "baseui/card";
+import { Card } from "baseui/card";
+import { TableBuilder, TableBuilderColumn } from "baseui/table-semantic/";
+
+const getVotingData = (data: any) => {
+  return [
+    {
+      name: "Joe Biden",
+      party: "Dem.",
+      count: data.VOTES.votes20_Joe_Biden,
+      percentage: `${(+data.VOTES.percentage20_Joe_Biden * 100).toFixed(1)}%`,
+      color: `#155a66`,
+      countyWiningColor: "rgba(21, 90, 102, 0.4)",
+    },
+    {
+      name: "Donald Trump",
+      party: "Rep.",
+      count: data.VOTES.votes20_Donald_Trump,
+      percentage: `${(+data.VOTES.percentage20_Donald_Trump * 100).toFixed(
+        1
+      )}%`,
+      color: `#8a0200`,
+      countyWiningColor: "rgba(138, 2, 0, 0.4)",
+    },
+  ];
+};
 
 export const CountyData = ({ data }: { data: any }) => {
   if (!data.VOTES) return null;
+  const formateNumber = new Intl.NumberFormat("en-US");
+  const votesData = getVotingData(data);
+
+  const maxValue = Math.max(...votesData.map((voteData) => +voteData.count));
+
+  const getBackgroundColor = (rowIndex: number) => {
+    return maxValue === +votesData[rowIndex].count
+      ? votesData[rowIndex].countyWiningColor
+      : "";
+  };
+
   return (
     <Card
       overrides={{
@@ -11,19 +46,37 @@ export const CountyData = ({ data }: { data: any }) => {
           style: {
             top: "1.5rem",
             right: "1.5rem",
-            width: "20rem",
+            width: "30rem",
             position: "absolute",
           },
         },
       }}
       title={`${data.VOTES.county} (${data.VOTES.state}) `}
     >
-      <StyledBody>
-        Trump:
-        {data.VOTES.percentage20_Donald_Trump}
-        Biden:
-        {data.VOTES.percentage20_Joe_Biden}
-      </StyledBody>
+      <TableBuilder
+        overrides={{
+          TableBodyRow: {
+            style: ({ $rowIndex }) => ({
+              borderLeft: `15px solid ${votesData[$rowIndex].color}`,
+              backgroundColor: getBackgroundColor($rowIndex),
+            }),
+          },
+        }}
+        data={votesData}
+      >
+        <TableBuilderColumn header="Candidate" id="name">
+          {(row) => row.name}
+        </TableBuilderColumn>
+        <TableBuilderColumn header="Party" id="party">
+          {(row) => row.party}
+        </TableBuilderColumn>
+        <TableBuilderColumn header="Votes" id="count">
+          {(row) => formateNumber.format(row.count)}
+        </TableBuilderColumn>
+        <TableBuilderColumn header="PCT." id="percentage">
+          {(row) => row.percentage}
+        </TableBuilderColumn>
+      </TableBuilder>
     </Card>
   );
 };
